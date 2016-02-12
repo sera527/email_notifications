1611d55c84717ffcbbec605ed05493bce6e6bd15
Сергій <serpos95@gmail.com>
2016-02-12 14:42:33 +0200

    add sql file

---
+++ createTables.sql
@@ -0,0 +1,18 @@
+/*Таблица объявлений*/
+CREATE TABLE IF NOT EXISTS items (
+id int(11) unsigned NOT NULL auto_increment, -- ID объявления
+user_id int(11) unsigned NOT NULL default 0, -- ID пользователя
+status tinyint(1) unsigned NOT NULL default 1, -- статус объявления
+title varchar(150) NOT NULL default '', -- заголовок объявления
+link text, -- ссылка на страницу просмотра объявления
+descr text, -- описание объявления
+publicated_to timestamp NOT NULL default '0000-00-00 00:00:00', -- срок окончания публикации объявления
+PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+/*Таблица пользователей*/
+CREATE TABLE IF NOT EXISTS users (
+id int(11) unsigned NOT NULL auto_increment, -- ID пользователя
+email text, -- Email пользователя
+PRIMARY KEY (id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
