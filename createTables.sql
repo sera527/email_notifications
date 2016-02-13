6b19e578e5510f747f41bb3e813b76b8d2a29d08
Сергій <serpos95@gmail.com>
2016-02-13 12:18:41 +0200

    add field to table "items"

--- createTables.sql
+++ createTables.sql
@@ -3,6 +3,15 @@ CREATE TABLE IF NOT EXISTS items (
 id int(11) unsigned NOT NULL auto_increment, -- ID объявления
 user_id int(11) unsigned NOT NULL default 0, -- ID пользователя
 status tinyint(1) unsigned NOT NULL default 1, -- статус объявления
+send tinyint(1) unsigned NOT NULL default 0,  -- маркер отправки email
+  /*
+  0 - никаких email-оповещений не отправлялось
+  5 - отправлено оповещение за 5 дней до окончания публикации объявления
+  2 - за 2 дня
+  1 - за 1 день
+
+  * После возобновления объявления нужно сбросить в 0
+  */
 title varchar(150) NOT NULL default '', -- заголовок объявления
 link text, -- ссылка на страницу просмотра объявления
 descr text, -- описание объявления
